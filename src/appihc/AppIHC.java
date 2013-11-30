de8f6effa354a61c26df6e5ba1671d4bb4380a01
Rodrigo Zanella Ribeiro <rodrigo.zanella@hotmail.com.br>
2013-11-30 13:15:56 -0200

    Integração com API gráfica (jMonkey)

--- src/appihc/AppIHC.java
+++ src/appihc/AppIHC.java
@@ -1,11 +1,44 @@
 package appihc;
 
+
+import com.jme3.app.SimpleApplication;
+import com.jme3.material.Material;
+import com.jme3.math.ColorRGBA;
+import com.jme3.math.Vector3f;
+import com.jme3.renderer.RenderManager;
+import com.jme3.scene.Geometry;
+import com.jme3.scene.shape.Box;
+
 /**
- * Application
- * @author Rodrigo Zanella Ribeiro
+ * test
+ * @author normenhansen
  */
-public class AppIHC {
+public class AppIHC extends SimpleApplication {
+
     public static void main(String[] args) {
-        // TODO code application logic here
+        AppIHC app = new AppIHC();
+        app.start();
+    }
+
+    @Override
+    public void simpleInitApp() {
+        Box b = new Box(1, 1, 1);
+        Geometry geom = new Geometry("Box", b);
+
+        Material mat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
+        mat.setColor("Color", ColorRGBA.Blue);
+        geom.setMaterial(mat);
+
+        rootNode.attachChild(geom);
+    }
+
+    @Override
+    public void simpleUpdate(float tpf) {
+        //TODO: add update code
+    }
+
+    @Override
+    public void simpleRender(RenderManager rm) {
+        //TODO: add render code
     }
 }
